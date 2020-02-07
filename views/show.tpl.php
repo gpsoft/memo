ce8a6ab74a0f0c6c0590b7f0064a6bf9ff65ca5a
kinokomochi <toadstomochi@gmail.com>
2020-02-07 12:46:17 +0900

    fix for show.tpl.php

--- views/show.tpl.php
+++ views/show.tpl.php
@@ -8,7 +8,7 @@
     <tr>
     <td>No.<?=$notes['id']; ?></td><br>
     <td>タイトル：<?=$notes['title']; ?></td><br>
-    <td>内容：<?=$notes['content']; ?></td><br>
+    <td>内容：<?=nl2br($notes['content']); ?></td><br>
     </tr>
 
     <p><a href='index.php'>一覧に戻る</a> | <a href='edit.php?id=<?=$id ?>'>編集</a> | <a href='delete.php?id=<?=$id ?>'>削除</a></p>
