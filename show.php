3a7380aab06145b46d35062310559ee3583bd385
kinokomochi <toadstomochi@gmail.com>
2020-02-08 23:00:42 +0900

    add 'htmlspecialchars'

--- show.php
+++ show.php
@@ -1,4 +1,12 @@
 <?php
+
+function h($str){
+    echo htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+function hbr($str){
+    echo nl2br(htmlspecialchars($str, ENT_QUOTES, 'UTF-8'));
+}
 if(isset($_REQUEST['id'])){
     $id = $_REQUEST['id'];
 }
--- views/show.tpl.php
+++ views/show.tpl.php
@@ -7,8 +7,8 @@
     <p></p>
     <tr>
     <td>No.<?=$notes['id']; ?></td><br>
-    <td>タイトル：<?=$notes['title']; ?></td><br>
-    <td>内容：<?=nl2br($notes['content']); ?></td><br>
+    <td>タイトル：<?=h($notes['title']); ?></td><br>
+    <td>内容：<br><?= hbr($notes['content']); ?></td><br>
     </tr>
 
     <p><a href='index.php'>一覧に戻る</a> | <a href='edit.php?id=<?=$id ?>'>編集</a> | <a href='delete.php?id=<?=$id ?>'>削除</a></p>
