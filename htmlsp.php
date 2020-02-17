bd176465cac3358543add4cdc8f4b7bd9640b18a
kinokomochi <toadstomochi@gmail.com>
2020-02-17 17:17:28 +0900

    fix HTMLescape

--- htmlsp.php
+++ htmlsp.php
@@ -1,8 +1,14 @@
 <?php
 function h($str){
-    echo htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+    //echo htmlspecialchars($str, ENT_QUOTES, 'UTF-8')
+    //returnを使うなら出力先でechoするのを忘れない
+
 }
 
 function hbr($str){
-    echo nl2br(h($str));
+    return nl2br(h($str));
+    //echo nl2br(htmlspecialchars($str, ENT_QUOTES, 'UTF-8'));
+    //return使うならこの範囲内でも上で作ったh関数を使って良い。出力先でecho
+    //echo使うなら上のh関数をここで使うとechoが２重になってしまい改行がうまくいかない
 }
