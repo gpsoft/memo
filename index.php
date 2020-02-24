1c39d040a804ba83fe4ea41ba7f2dbecb98136f5
gpsoft <etoh.gpsoft@gmail.com>
2020-02-24 22:26:25 +0900

    Refactor index page

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-$logger = new Logger('notes');
-$logger->pushHandler(new StreamHandler(__DIR__ . '/log/debug.log', Logger::DEBUG));
-
-//$logger->pushHandler(new MemoryUsageProcessor);
-$logger->debug('デバッグ　メッセージ　');
-$logger->debug(__DIR__);
-$logger->warning('警告レベル　　の　メッセージ　　　');
-$logger->error('EROORです　');
-
-
-
-
-require_once ('config.php');
 $pdo = connectDB();
 $sql = 'SELECT * FROM notes';//一覧表示したいので全部取得
+logD($sql, 'SQL');
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $notes = [];//空の配列の準備　必要ある？
@@ -26,10 +11,11 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     $notes[] = $row;
 }
 //fetchは１行しか取得しない　引数で取得方法を指定（今回は連想配列）
+logD(count($notes), 'Total');
 $pdo = null;
 $stmt = null;
-$message = 'メモ一覧';
-    
 
-require_once 'htmlsp.php';
+// indexページ
+// input: $message, $notes
+$message = 'メモ一覧';
 require_once 'views/index.tpl.php';
--- views/index.tpl.php
+++ views/index.tpl.php
@@ -3,14 +3,14 @@
 <?php include('header.inc.php') ?>
 
 <body>
-    <h2><?= $message; ?></h2>
+    <h2><?= h($message); ?></h2>
     <p></p>
 
     <?php foreach($notes as $note){ ?>
         <p><a href="show.php?id=<?= $note['id']; ?>"><?php echo h($note['title']); ?><br></a></p>
     <?php } ?>
 
-    <p><a href="new.php"> 新規メモ</a></p>
+    <p><a href="new.php">新規メモ</a></p>
 
 <?php include('footer.inc.php') ?>
 </body>
