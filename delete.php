8ef3d791dd26f06bcd606bdf3e75c326b96f8e92
gpsoft <etoh.gpsoft@gmail.com>
2020-02-24 23:19:32 +0900

    Refactoring: deleting note

--- delete.php
+++ delete.php
@@ -1,30 +1,24 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
 if(isset($_REQUEST['id'])){
     $id = $_REQUEST['id'];
+    logD($id, 'id');
 }else{
     //formタグを使っていないからurlに直接delete入力しても削除できる
     header('Location: show.php?id='.$id);
     exit();
 }
 
-require_once ('config.php');
 $pdo = connectDB();
-
-
 $sql = 'DELETE FROM notes WHERE id = :id;';
+logD($sql, 'SQL');
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 
-
-
-
-
 $pdo = null;
-$stmt = null;    
+$stmt = null;
 
 // print_r($_REQUEST);
 header('Location: index.php');
-
-
-    
