d0fe4d6235cfbeb3da6ff03038136ff565f55c2f
gpsoft <etoh.gpsoft@gmail.com>
2020-02-27 18:43:16 +0900

    Refactor update.php

--- update.php
+++ update.php
@@ -1,5 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
 if(!empty($_REQUEST)){
+    logD($_REQUEST, '$_REQUEST');
     if(is_int($_REQUEST['id'])){
         header('Location: index.php');
         exit();
@@ -7,7 +10,7 @@ if(!empty($_REQUEST)){
     if($_REQUEST['title'] == ''){
         $error['title'] = 'blank';
     }
-    if(mb_strlen($_REQUEST['title']) >= 255){
+    if(mb_strlen($_REQUEST['title']) > 255){
         $error['title'] = 'length';
     }
     if($_REQUEST['content'] == ''){
@@ -19,42 +22,40 @@ if(!empty($_REQUEST)){
     $notes['content'] = $_REQUEST['content'];
 }
 if(!empty($error)){
-    // header("Location: edit.php?title=".$error['title']."&content=".$error['content']."&request_t=".$_REQUEST['title']."&request_c=".$_REQUEST['content']."&id=".$_REQUEST['id']);
-    // exit();
+    logD($error, 'error');
     require_once 'views/edit.tpl.php';
+    exit();
 }
-if(empty($error)){
-    if(isset($_REQUEST['submit'])){
-        $id =$_REQUEST['id'];
-        $title = $_REQUEST['title'];
-        $content = $_REQUEST['content'];
-    }else{
-        //$idに数値以外の文字列が入っても表示されてしまいリダイレクトしない。
-        header('Location: show.php?id='.$id);
-        exit();
-    }
+if(isset($_REQUEST['submit'])){
+    $id =$_REQUEST['id'];
+    $title = $_REQUEST['title'];
+    $content = $_REQUEST['content'];
+}else{
+    //$idに数値以外の文字列が入っても表示されてしまいリダイレクトしない。
+    header('Location: show.php?id='.$id);
+    exit();
+}
 
-require_once ('config.php');
 $pdo = connectDB();
 
 $sql = 'UPDATE notes SET title = :title, content =:content WHERE id = :id' ;
+logD($sql, 'SQL');
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':title', $title, PDO::PARAM_STR);
 $stmt->bindValue(':content', $content, PDO::PARAM_STR);
 $stmt->execute();
 
+//デバッグ用に、SELECTしてみる(本来は不要)。
 $sql = 'SELECT * FROM notes';
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
-
 $notes = $stmt->fetch(PDO::FETCH_ASSOC);
-
+logD($note);
 
 $pdo = null;
 $stmt = null;
 
-//require_once('views/edit.tpl.php');
-
+logD('Redirecting to: show.php?id='.$id);
 header('Location: show.php?id='.$id);
-}
+
