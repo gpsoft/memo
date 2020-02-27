5df5ca435b722691596d9d8f6e949bc3fb94b9ab
gpsoft <etoh.gpsoft@gmail.com>
2020-02-27 18:27:35 +0900

    Refactor edit page

--- edit.php
+++ edit.php
@@ -1,46 +1,26 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-$logger = new Logger('notes');
-$logger->pushHandler(new StreamHandler(__DIR__ . '/log/debug.log', Logger::DEBUG));
-
-//$logger->pushHandler(new MemoryUsageProcessor);
-$logger->debug('デバッグ　メッセージ　');
-$logger->debug(__DIR__);
-$logger->warning('警告レベル　　の　メッセージ　　　');
-$logger->error('EROORです　');
-
-
-
 if(isset($_REQUEST['id'])){
     $id = $_REQUEST['id'];
-}//else{
-//     //echo "不正なアクセスです";
-//     header('Location: index.php');
-//     exit();
-// }
-require_once ('config.php');
-$pdo = connectDB();
+}else{
+    //echo "不正なアクセスです";
+    header('Location: index.php');
+    exit();
+}
 
-$sql = 'SELECT id, title, content FROM notes 
-        WHERE id = :id ';
+$pdo = connectDB();
+$sql = 'SELECT id, title, content FROM notes WHERE id = :id ';
+logD($sql, 'SQL');
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
-$notes = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-
+$note = $stmt->fetch(PDO::FETCH_ASSOC);
+logD($note);
 $pdo = null;
 $stmt = null;
 
+// editページ
+// input: $message, $note, $error
 $message = 'Memo Update';
-    
-
-
 require_once 'views/edit.tpl.php';
-
-    
--- views/edit.tpl.php
+++ views/edit.tpl.php
@@ -3,17 +3,12 @@
 <?php include('header.inc.php') ?>
 
 <body>
-    <!-- <?php var_dump($_REQUEST); ?>
-    <br>
-    <?php var_dump($error); ?>
-    <br>
-    <?php var_dump($notes); ?> -->
-    <h2><?= $message; ?></h2>
+    <h2><?= h($message); ?></h2>
     <p></p>
     <form action="update.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $notes['id']; ?>">
+        <input type="hidden" name="id" value="<?php echo $note['id']; ?>">
         <label for="title">タイトル</label><br>
-        <input type="text" name="title" value="<?php echo $notes['title']; ?>">
+        <input type="text" name="title" value="<?= h($note['title']); ?>">
         <br>
         <?php if($error['title'] == 'blank'): ?>
         <?php echo "タイトルを入力してください"; ?>
@@ -24,7 +19,7 @@
         <?php endif ?>
         <p></p>
         <label>テキスト</label><br>
-        <textarea name="content" cols="40" rows="10"><?php echo $notes['content']; ?></textarea>
+        <textarea name="content" cols="40" rows="10"><?= h($note['content']); ?></textarea>
         <br>
         <?php if($error['content'] == 'blank'): ?>
         <?php echo "テキストを入力してください"; ?>
@@ -32,8 +27,7 @@
         <p></p>
         <button type="submit" name="submit" value="submit">変更する</button>
 
-    <!-- <?php print_r($notes); ?> -->
-    <p><a href="index.php"> 一覧に戻る</a></p>
+    <p><a href="index.php">一覧に戻る</a></p>
 
 <?php include('footer.inc.php') ?>
 </body>
